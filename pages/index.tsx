7fdcadcccac50c7eea86b05024d63a48077627ac
Albar00n <mgrushi73@gmail.com>
2022-06-15 14:02:30 +0400

    site-verification

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,27 +7,38 @@ import Navbar from "../components/navbar/index"
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Beyin</title>
-        <meta name="description" content="Beyin Digital Technology" />
-         {/* <meta name="description" content="Watch TV shows and movies online. Stream TV episodes of South Park, Empire, SNL, Modern Family and popular movies on your favorite devices. Start your free trial now. Cancel anytime." /> */}
-         <meta data-rh="true" name="google" content="notranslate" />
-         <meta data-rh="true" name="keywords" content="Businesses card, virtual business card, online business card" />
-         <meta data-rh="true" name="keywords" content="Augmented Reality business card,AR businesses card,VR businesses card,Virtual Reality Business card,Extended reality business card" />
-         <meta data-rh="true" name="robots" content="index, follow"/>
-         <meta name="Linkedin:site" content="@beyin-tech" />
-         <meta name="instagram:site" content="@beyin.me" />
-          <link rel="icon" href="/favicon.ico" />
-      </Head>
+		<div className={styles.container}>
+			<Head>
+				<title>Beyin</title>
+				<meta name="description" content="Beyin Digital Technology" />
+				<meta
+					name="google-site-verification"
+					content="gFgDtzR0i6ZwRv2Bu3dj-Odvct12dr19DfqLunnasUU"
+				/>
+				{/* <meta name="description" content="Watch TV shows and movies online. Stream TV episodes of South Park, Empire, SNL, Modern Family and popular movies on your favorite devices. Start your free trial now. Cancel anytime." /> */}
+				<meta data-rh="true" name="google" content="notranslate" />
+				<meta
+					data-rh="true"
+					name="keywords"
+					content="Businesses card, virtual business card, online business card"
+				/>
+				<meta
+					data-rh="true"
+					name="keywords"
+					content="Augmented Reality business card,AR businesses card,VR businesses card,Virtual Reality Business card,Extended reality business card"
+				/>
+				<meta data-rh="true" name="robots" content="index, follow" />
+				<meta name="Linkedin:site" content="@beyin-tech" />
+				<meta name="instagram:site" content="@beyin.me" />
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
 
-    <main>
-      <Navbar />
-      <Container>
-     <Layout />
-     </Container>
-    </main>
-
-    </div>
-  )
+			<main>
+				<Navbar />
+				<Container>
+					<Layout />
+				</Container>
+			</main>
+		</div>
+	);
 }
